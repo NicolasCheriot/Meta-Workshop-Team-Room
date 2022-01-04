64c57825d8773a692b707906cf8713b12ccbdd27
NicolasCheriot <coaching@nicolascheriot.fr>
2022-01-04 22:20:01 +0100

    MAJ

--- Generic_Home_1_Layer_1_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Generic_Home_1_Layer_1_32x32" tilewidth="32" tileheight="32" tilecount="182" columns="14">
- <image source="src/assets/6_Home_Designs/Generic_Home_Designs/32x32/Generic_Home_1_Layer_1_32x32.png" width="448" height="428"/>
-</tileset>
